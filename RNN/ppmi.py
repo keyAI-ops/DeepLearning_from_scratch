a4e094a75eee910374aaa2ecc30fb6c5b5b4efda
keyAI-ops <ckyou1207@gmail.com>
2023-05-05 16:18:07 +0900

    scratch_common

---
+++ RNN/ppmi.py
@@ -0,0 +1,26 @@
+#%%
+import numpy as np
+
+def ppmi (C, verbose=False, eps=1e-8):  # C는 동시발생 행렬, verbose 진행상황 출력 여부를 알려주는 플래그
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+    
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[j]*S[i]) + eps)
+            M[i, j] = max(0, pmi)  # 이 부분을 통해 -inf로 가는 문제를 해소
+            
+            if verbose:
+                cnt += 1
+                if cnt % (total//100 +1) == 0:
+                    print('%.1f%% 완료' % (100*cnt/total))
+    
+    return M
+# %%
+from common.util import preprocess, create_co_matrix, cos_similarity
+
+text
+# %%
