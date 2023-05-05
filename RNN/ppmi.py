6e6b0fbc25b5b011f508fa26a989323527789b61
keyAI-ops <ckyou1207@gmail.com>
2023-05-05 16:22:20 +0900

    ppmi

--- RNN/ppmi.py
+++ RNN/ppmi.py
@@ -1,27 +1,31 @@
-#%%
+# %%
 import numpy as np
 
-def ppmi (C, verbose=False, eps=1e-8):  # C는 동시발생 행렬, verbose 진행상황 출력 여부를 알려주는 플래그
+
+def ppmi(C, verbose=False, eps=1e-8):  # C는 동시발생 행렬, verbose 진행상황 출력 여부를 알려주는 플래그
     M = np.zeros_like(C, dtype=np.float32)
     N = np.sum(C)
     S = np.sum(C, axis=0)
     total = C.shape[0] * C.shape[1]
     cnt = 0
-    
+
     for i in range(C.shape[0]):
         for j in range(C.shape[1]):
-            pmi = np.log2(C[i, j] * N / (S[j]*S[i]) + eps)
+            pmi = np.log2(C[i, j] * N / (S[j] * S[i]) + eps)
             M[i, j] = max(0, pmi)  # 이 부분을 통해 -inf로 가는 문제를 해소
-            
+
             if verbose:
                 cnt += 1
-                if cnt % (total//100 +1) == 0:
-                    print('%.1f%% 완료' % (100*cnt/total))
-    
+                if cnt % (total // 100 + 1) == 0:
+                    print("%.1f%% 완료" % (100 * cnt / total))
+
     return M
+
+
 # %%
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 from common.util import preprocess, create_co_matrix, cos_similarity
 
 text = "you say goodbye and I say hello"
@@ -32,7 +36,7 @@ W = ppmi(C)
 
 np.set_printoptions(precision=3)
 print(C)
-print('-'*50)
+print("-" * 50)
 print(W)
 
 # %%
